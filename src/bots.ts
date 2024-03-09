c62dbb0695bd093e2885c3a716341b0059b6cde9
jweboy <jweboy0630@gmail.com>
2024-03-10 01:18:45 +0800

    feat: 更新token

--- src/bots.ts
+++ src/bots.ts
@@ -1,6 +1,8 @@
 import TelegramBot from "node-telegram-bot-api";
 import "dotenv/config";
 
+console.log(process.env.TOKEN);
+
 const bot = new TelegramBot(process.env.TOKEN!, { polling: true });
 
 bot.onText(/\/start/, (msg) => {
@@ -9,6 +11,7 @@ bot.onText(/\/start/, (msg) => {
 
 bot.onText(/\/my/, async (msg) => {
   const data = await bot.getMe();
+  console.log("my msg=>", JSON.stringify(data, null, 2));
   bot.sendMessage(msg.chat.id, JSON.stringify(data, null, 2));
 });
 
@@ -23,3 +26,11 @@ bot.onText(/\/commands/, async (msg) => {
 });
 
 console.log("Bots is running");
+
+process.on("uncaughtException", function (error) {
+  console.log("\x1b[31m", "Exception: ", error, "\x1b[0m");
+});
+
+process.on("unhandledRejection", function (error: any, p) {
+  console.log("\x1b[31m", "Error: ", error.message, "\x1b[0m");
+});
