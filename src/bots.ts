1f5e9c6f58d5d2f9483c8f06a8bebff856c3c840
jweboy <jweboy0630@gmail.com>
2024-03-10 01:00:40 +0800

    初始化项目

---
+++ src/bots.ts
@@ -0,0 +1,23 @@
+import TelegramBot from "node-telegram-bot-api";
+import "dotenv/config";
+
+const bot = new TelegramBot(process.env.TOKEN!, { polling: true });
+
+bot.onText(/\/start/, (msg) => {
+  bot.sendMessage(msg.chat.id, "Welcome 👏👏👏");
+});
+
+bot.onText(/\/my/, async (msg) => {
+  const data = await bot.getMe();
+  bot.sendMessage(msg.chat.id, JSON.stringify(data, null, 2));
+});
+
+bot.onText(/\/updates/, async (msg) => {
+  const data = await bot.getUpdates();
+  bot.sendMessage(msg.chat.id, JSON.stringify(data, null, 2));
+});
+
+bot.onText(/\/commands/, async (msg) => {
+  const data = await bot.getMyCommands();
+  bot.sendMessage(msg.chat.id, JSON.stringify(data, null, 2));
+});
